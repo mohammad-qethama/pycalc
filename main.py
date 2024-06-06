0c1e569c3c4d60b06f687f747c9b6a2e0dafd04e
mohammad-qethama <mhmd.a@protonmail.com>
2024-06-06 15:01:43 +0300

    done stack-solution for console input- delted all old solution no new branch

--- main.py
+++ main.py
@@ -2,33 +2,42 @@
 import re
 from calc_params import CalcParameters
 from calc_methods import CalcMethods
-
+from stack_class import StackClass
 
 while True:
    
     
-    try: 
-        first_num = input('Input first Number\n')
-        first_num = int( first_num)
-        
-        second_num = input('Enter second Number\n')
-        second_num = int(second_num)
-        operation = input('input * / + - to  perfume the symbol corresponding math operation\n')
-        params_object = CalcParameters(first_number=first_num,second_number=second_num,operation=operation)
-        methods_obj = CalcMethods()
-        # regex.compile can be used to get better regex strings
-        if (not re.match('[\* \/ \- \+]',operation)):
-            raise Exception('Please Enter Valid Operation symbol * or - or / or + ')
-    except Exception as e :
-        print('error: ',e)
-        continue
+
+        # first_num = input('Input first Number\n')
+        # first_num = int(first_num)
     try:
-        print(methods_obj.calculate(params_object.first_number,params_object.second_number,params_object.operation) )
-    except ZeroDivisionError:
-        print('Math Error:Cant divide by 0 ')
-    except :
+        expression = input('enter your mathematical expression:\n')
+        stack_object = StackClass()
+        print(stack_object.pushToStacks(expression))
+        
+        
+        
+        # second_num = input('Enter second Number\n')
+        # second_num = int(second_num)
+        # operation = input('input * / + - to  perfume the symbol corresponding math operation\n')
+        # params_object = CalcParameters(first_number=first_num,second_number=second_num,operation=operation)
+        # methods_obj = CalcMethods()
+        
+        # regex.compile can be used to get better regex strings
+        # if (not re.match('[\* \/ \- \+]',operation)):
+        #     raise Exception('Please Enter Valid Operation symbol * or - or / or + ')
+    except Exception as e:
         print('Error',e)
+        
+    # print('error: ',e)
+    # continue
+    # try:
+    #     print(methods_obj.calculate(params_object.first_number,params_object.second_number,params_object.operation) )
+    # except ZeroDivisionError:
+    #     print('Math Error:Cant divide by 0 ')
+    # except :
+    #     print('Error',e)
     
-    del params_object
-    del methods_obj
+    # del params_object
+    # del methods_obj
     
